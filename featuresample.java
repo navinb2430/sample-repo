e83f9dd66565b2d75906c0edaeb14a8397a23761
nani <navinb2430@gmail.com>
2023-09-05 20:51:08 +0000

    This is feature samplefeature.java file first commit

---
+++ featuresample.java
@@ -0,0 +1,13 @@
+//This is a simple java program
+//fileName: "HelloWorld.java"
+
+class HelloWorld {
+          //your program begins with a call to main().
+          //prints "Hello Edureka" to the terminal windows
+          public static void main(string args[])
+          {
+                  system.out.println("Hello this is first samplefeature.java!!!");
+          }
+
+}
+
